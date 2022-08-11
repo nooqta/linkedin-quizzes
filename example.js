6923a94beca755447aa04299ec3fedf335f2daec
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-08-11 12:48:36 +0100

    fix: update example

--- example.js
+++ example.js
@@ -1,2 +1,4 @@
-const lkq = require('linkedin-quizzes');
+const lkq = require('./index.js');
+// You should import the module and use it as follows:
+// const lkq = require('linkedin-quizzes');
 lkq.newQuiz();
